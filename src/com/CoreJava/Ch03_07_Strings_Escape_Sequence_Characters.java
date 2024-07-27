a33f80f0c3da04adaefe6163f04782789be10a6d
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-07-27 22:48:01 +0530

    Escape Sequences Characters in  Java Strings

---
+++ src/com/CoreJava/Ch03_07_Strings_Escape_Sequence_Characters.java
@@ -0,0 +1,32 @@
+package com.CoreJava;
+
+public class Ch03_07_Strings_Escape_Sequence_Characters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		// ESCAPE SEQUENCE CHARACTERS :-	
+		                               /* A character with a backslash (\) just before 
+		                                * it is an escape sequence or escape character. 
+		                                * We use escape characters to perform some specific task. 
+		                                * The total number of escape sequences or escape characters in Java is 8. 
+		                                * Each escape character is a valid character literal.
+		                                */
+		
+		
+		// Sequence of Characters after backslash '\' = Escape sequence characters.
+		// Escape Sequence Characters consist of more than one Characters but represents
+		// one character used within the Strings.
+		
+		// Example : \n, \t, \', \\, etc.
+		
+		
+		
+		System.out.println("I am escape sequence \" react as double quote");
+		System.out.println("I am escape sequence \\ also as a double quote");
+		System.out.println("I am new line escape sequence \n given new line");
+	}
+
+}
