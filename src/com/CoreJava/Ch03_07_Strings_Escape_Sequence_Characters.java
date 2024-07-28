39e889156f1adc882c0c03026d4da6ae6c3619ed
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-07-28 11:44:22 +0530

    Updated Ch03_07_Strings_Escape_Sequence_Characters.java

--- src/com/CoreJava/Ch03_07_Strings_Escape_Sequence_Characters.java
+++ src/com/CoreJava/Ch03_07_Strings_Escape_Sequence_Characters.java
@@ -27,6 +27,64 @@ public class Ch03_07_Strings_Escape_Sequence_Characters {
 		System.out.println("I am escape sequence \" react as double quote");
 		System.out.println("I am escape sequence \\ also as a double quote");
 		System.out.println("I am new line escape sequence \n given new line");
+
+
+		// Types of ESCAPE SEQUENCES
+		
+		
+		// 1.  \t or Tab
+		
+		System.out.println("Hello! \t My name is Rahul"); // Added Tab or big space between a String
+		                          // Output is : Hello!		My name is Rahul.
+		
+		
+		
+		// 2. \b or Backspace
+		
+		System.out.println("Hello! \b My name is Rahul");
+		
+		
+		
+		// 3. \n or New Line
+		
+		System.out.println("Hello! \n My name is Rahul"); // Constructed New Line Between desired String.
+		                        /* Output : Hello!
+		                                    My name is Rahul  */
+		
+		
+		
+		// 4. \r or Carriage Return
+		
+		System.out.println("Hello! \r My Name is Rahul");
+		
+		
+		
+		// 5. \' or Single Quote Character
+		
+		System.out.println("Hello! \'My name is Rahul\'"); // Added single quote character in the line.
+		
+		
+		
+		// 6. \" or Double Quote Character
+		
+		System.out.println("Hello! \"My name is Rahul\""); // Added Double quotes around desired Code.
+		
+		
+		
+		// 7. \f or Form Feed
+		System.out.println("Hello! \f My name is Rahul");
+		
+		
+		
+		// 8. \\ or backslash
+		
+		System.out.println("Hello \\ My name is Rahul"); // Added a Backslash Character in the Text.
+		
+		
+		
+		
+		
+		// Description about few Escape Sequences, we will learn later.
 	}
 
 }
