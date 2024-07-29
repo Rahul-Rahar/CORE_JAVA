bd6b94422358167f52754bbef0f1c79b7ec5624f
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-07-29 12:37:11 +0530

    CONDITIONAL STATEMENTS

---
+++ src/com/CoreJava/Ch04_00_Conditional_Statements.java
@@ -0,0 +1,34 @@
+package com.CoreJava;
+
+public class Ch04_00_Conditional_Statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// CONDITIONAL STATEMENTS IN JAVA
+		
+		// Conditional Statements executes a piece of code when certain conditions are met.
+		
+		// like...
+		/* Sometimes we want to watch comedy videos on Youtube if the day is Sunday.
+		 * 
+		 * Sometimes, we order junk food if it is our friend's birthday in the hostel.
+		 * 
+		 * Might want to buy Umbrella if its raining and have the money.
+		 * 
+		 * Order the meal if something is favourite and listed on the menu.
+		 * 
+		 */
+		
+		
+		
+		// In Java, we can execute instructions on a Condition being met.
+		
+		
+		// Types of Conditional Statements in Java 
+		// 1. If-else Statements                  2. Switch Statements
+
+	}
+
+}
