7bcf7d2574046cf51261267fe72bf303151836b8
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-08-01 16:43:51 +0530

    Ch04 Conditional Practice Que4

---
+++ src/com/CoreJava/Ch04_Practice_Conditionals_04.java
@@ -0,0 +1,36 @@
+package com.CoreJava;
+
+public class Ch04_Practice_Conditionals_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// WHAT WILL BE THE OUTPUT OF THE PROGRAM
+		
+		/*
+		   int a = 10;
+		   if(a=11)
+		        System.out.println("I am 11");
+	        
+	        
+	       else 
+	            System.out.println("I am not 11")
+		 */
+
+		// THE OUTPUT IS not Obtained Because There are too many errors in this program. 
+		// So the Correct code is:--
+		
+		
+		int a = 10;
+		if(a==11) {
+			System.out.println("I am 11");
+		}
+		
+		else {
+			System.out.println("I am not 11");
+		}
+		
+		// And Hence the Output of Corrected Code is "I am not 11" as a=10.
+	}
+
+}
